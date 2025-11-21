1bfe7141509ac88539debfec4b5d81fae07c9a5f
phatsaccount <phatge2005@gmail.com>
2025-11-21 22:49:42 +0700

    Revert "update cloudfront url"
    This reverts commit ac36e1799ce5867e1dc40db9742ac1c9821fc376.

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,7 +4,7 @@
 import { fetchAuthSession } from 'aws-amplify/auth';
 
 const API_GATEWAY_URL = import.meta.env.VITE_API_GATEWAY_URL || 'https://8rzkjedi72.execute-api.ap-southeast-1.amazonaws.com/v1/upload-url'
-const CLOUDFRONT_URL = import.meta.env.VITE_CLOUDFRONT_URL || 'https://d14vg5o4yx9zqx.cloudfront.net'
+const CLOUDFRONT_URL = import.meta.env.VITE_CLOUDFRONT_URL || 'YOUR_CLOUDFRONT_URL'
 
 /**
  * BƯỚC 3: Hàm phụ trợ để lấy token hiện tại
